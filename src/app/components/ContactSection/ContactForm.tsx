742a3db931bf9f3ead9e930100d4f02aebf41c11
Ben Mahanloo <benmahanloo@gmail.com>
2024-12-06 16:08:18 -0800

    undo changes

--- src/app/components/ContactSection/ContactForm.tsx
+++ src/app/components/ContactSection/ContactForm.tsx
@@ -1,25 +1,57 @@
 "use client";
-import React, { useState } from "react";
+import React, { FormEvent, useState } from "react";
 
 const ContactForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
+  const validateEmail = (email: string) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
+  const validateName = (name: string) => {
+    return name.length > 0;
+  };
+  const validateMessage = (message: string) => {
+    return message.length > 0;
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    if (!validateEmail(email)) {
+      alert("Please enter a valid email address");
+      return;
+    }
+    if (!validateName(name)) {
+      alert("Please enter your name");
+      return;
+    }
+    if (!validateMessage(message)) {
+      alert("Please enter a message");
+      return;
+    }
+    setSubmitted(true);
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        setSubmitted(false);
+        setName("");
+        setEmail("");
+        setMessage("");
+        resolve(null);
+      }, 5000);
+    });
+  };
+
   return (
-    <div className="w-full flex justify-center border-2 border-primary p-12">
+    <div className="w-full flex justify-center border-2  border-primary p-12">
       {submitted ? (
         <h1 className="text-4xl font-bold text-white">
           Thank you for your message, we will get back to you soon!
         </h1>
       ) : (
-        <form
-          className="w-full max-w-lg"
-          name="contact-form"
-          method="POST"
-          action="/"
-          data-netlify="true" // This enables Netlify form handling
-          onSubmit={() => setSubmitted(true)} // Handle the submitted state here
-        >
-          <input type="hidden" name="form-name" value="contact-form" />
+        <form className="w-full max-w-lg " onSubmit={handleSubmit}>
           <div className="flex flex-col mb-4">
             <label htmlFor="name" className="text-white mb-2">
               Name
@@ -30,6 +62,8 @@ const ContactForm = () => {
               name="name"
               placeholder="Enter your name"
               className="p-2 rounded-sm border-2 border-transparent outline-none focus:border-primary transition-all"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="flex flex-col mb-4">
@@ -42,6 +76,8 @@ const ContactForm = () => {
               name="email"
               placeholder="Enter your email"
               className="p-2 rounded-sm border-2 border-transparent outline-none focus:border-primary transition-all"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="flex flex-col mb-4">
@@ -53,11 +89,13 @@ const ContactForm = () => {
               name="message"
               placeholder="Enter your message"
               className="p-2 rounded-sm border-2 border-transparent outline-none focus:border-primary transition-all"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
             ></textarea>
           </div>
           <button
             type="submit"
-            className="mt-2 text-white bg-primary hover:bg-secondary transition-all py-2 px-4 rounded-sm"
+            className="mt-2 text-white bg-primary hover:bg-secondary transition-all  py-2 px-4 rounded-sm "
           >
             Submit
           </button>
