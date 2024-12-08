6f63a2b74192cec182519b2f657d1a5e41d84564
Ben Mahanloo <benmahanloo@gmail.com>
2024-12-07 17:17:59 -0800

    u

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,18 +14,21 @@ const About = () => {
         <div className="text-lg mt-4 flex flex-col gap-2 max-w-3xl text-pretty">
           <p className="indent-10">
             Canspan Construction is a construction company located in North
-            Vancouver, BC, Canada. We have expierience building on the North
-            Shore for over 40 years, dealing in all types of construction from
-            industrial to residential. We specialise in residential renovations
-            and laneway homes.
+            Vancouver, BC, Canada. We have experience building in the Lower
+            Mainland for over 40 years, dealing in all types of construction
+            from industrial to residential. We specialize in new residential
+            construction and laneway homes.
           </p>
         </div>
+        <br />
+        <br />
         <h1 className="text-4xl font-bold">Why Us</h1>
         <div className="text-lg mt-4 flex flex-col gap-2 max-w-3xl text-pretty">
           <p className="indent-10">
             We understand that you have a busy schedule and that construction
             can be stressful. When you hire us as your building contractor, you
             deal solely with us to make the process simple and stress free. We
+            guide you through the entire process from design to construction. We
             deal with other sub-contractors, engineers, city officials and
             architects on your behalf so you only have one point of contact
             throughout the whole building process.
